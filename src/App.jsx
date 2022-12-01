3df2c709e5cde92d21b50a1c3e223693540f80c7
Lucas-Debiais <lucas.debiais11@gmail.com>
2022-12-01 12:14:24 +0100

    Feat: add greetings and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import {useState, useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -14,31 +14,26 @@ function Counter() {
     )
 
 }
-function App() {
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React !</h1>
-      <div className="card">
-        <Counter />
-        <Counter />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const Greetings = ({highlighted, name, smiley, color}) => {
+    return (
+        <div className={highlighted ? 'highlighted' : ''} style={{color}}>Hello {name} {smiley}</div>
+    )
+}
+
+function App() {
+    const [highlighted, setHighlighted] = useState(false)
+    return (
+        <div className="App">
+            <div className="card">
+                <Counter/>
+                <Counter/>
+                <Greetings highlighted={highlighted} name={'Les M1'} smiley={'😁'}/>
+                <Greetings color={'blue'} name={'La classe'} smiley={'😆'}/>
+                <button onClick={() => setHighlighted(highlighted => !highlighted)}>toggle highlight</button>
+            </div>
+        </div>
+    )
 }
 
 export default App
